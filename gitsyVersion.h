435d51e96a8f64c9d5f6f9c6a35e5c93c8004817
tackelua <tackelua@gmail.com>
2017-03-17 13:42:13 +0700

    Update gitsyVersion.h

--- gitsyVersion.h
+++ gitsyVersion.h
@@ -3,8 +3,8 @@
 #ifndef _GITSY_VERSION_h
 #define _GITSY_VERSION_h
 
-#define _version "v0.1 20170317"
+#define _version "v0.1.1 20170317"
 static bool forceUpdate = false;
-//<url>http://gitsy.16mb.com/gitsy/firmwares/Gitsy_simple.bin</url>
+//<url>https://firebasestorage.googleapis.com/v0/b/blynk-database.appspot.com/o/Gitsy_simple.bin?alt=media&token=4f3052a2-01a8-4bf8-a870-bc91728153f8</url>
 
 #endif
